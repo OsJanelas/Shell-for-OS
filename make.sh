622b567e1dd9fcdc4b3440b4e09f048ad562154e
OsJanelas <osjanelas@gmail.com>
2026-02-28 11:10:13 -0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,1 +1,11 @@
+echo "compiling kernel"
 nasm -felf   kernel.asm        -o kernel.o
+
+echo "compiling userspace"
+i686-elf-gcc -ffreestanding -m32 -g -c shell.c       -o shell.o
+
+echo "linking"
+ld -m elf_i386 -T linker.ld -o fullkernel kernel.o shell.o
+
+echo "Generating IMG File"
+grub-mkrescue -o ShellExample.img Shell/
