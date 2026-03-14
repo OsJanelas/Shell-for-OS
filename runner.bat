ab5930701ffa56ed777b327532cc8d306aa8351b
OsJanelas <osjanelas@gmail.com>
2026-03-14 13:21:27 -0300

    Delete runner.bat

--- runner.bat
+++
@@ -1,2 +0,0 @@
-qemu-img create -f raw disk.raw 256M
-qemu-system-x86_64 -device sb16 -net nic,model=rtl8139 -net user -drive file=ShellExample.img,format=raw,if=ide,index=0 -drive file=disk.raw,format=raw,if=ide,index=2
